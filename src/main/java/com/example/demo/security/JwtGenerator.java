40923cd1533e6b6ab827a2b617cf7d9f1cf8c79e
IAPopov <ivan.popov5@dxc.com>
2023-11-25 16:04:41 +0100

    Added gitignore and few test jwt files

--- src/main/java/com/example/demo/security/JwtGenerator.java
+++ src/main/java/com/example/demo/security/JwtGenerator.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
--- src/main/java/com/example/demo/security/JwtVerifier.java
+++ src/main/java/com/example/demo/security/JwtVerifier.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
