8ef7d59ddd7d30111f76a985dd15e38409064415
IAPopov <ivan.popov5@dxc.com>
2023-11-25 16:47:15 +0100

    Added organization and contact table. Aded test data for organization

---
+++ src/main/java/com/example/demo/util/TestDataInitializer.java
@@ -0,0 +1,11 @@
+package com.example.demo.util;
+
+public class TestDataInitializer {
+
+    private static final String ORGANIZATION_INSERT = "INSERT INTO organization (type, name, address, logoUrl)\n" +
+            "VALUES ('Type of Organization', 'Organization Name', 'Organization Address', 'http://example.com/logo.png');"
+
+    private static void generateOrganizationData() {
+
+    }
+}
---
+++ src/main/resources/static/schema_creation.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "organization" (
+                                "id" SERIAL PRIMARY KEY,
+                                "type" varchar,
+                                "name" varchar,
+                                "address" varchar,
+                                "logoUrl" varchar,
+                                "name_vector_eng" tsvector GENERATED ALWAYS AS (to_tsvector('english', name)) STORED,
+                                "address_vector_eng" tsvector GENERATED ALWAYS AS (to_tsvector('english', address)) STORED
+);
+
+CREATE TABLE "contact" (
+                           "id" SERIAL PRIMARY KEY,
+                           "type" varchar,
+                           "value" varchar,
+                           "organization_id" bigint,
+                           "value_vector_eng" tsvector GENERATED ALWAYS AS (to_tsvector('english', value)) STORED
+);
+
+CREATE TABLE "organization_common_vector" (
+                                              "organization_id" bigint,
+                                              "search_vector" tsvector
+);
+
+ALTER TABLE "contact" ADD FOREIGN KEY ("organization_id") REFERENCES "organization" ("id");
+
+ALTER TABLE "organization_common_vector" ADD FOREIGN KEY ("organization_id") REFERENCES "organization" ("id");
