14d0bba72ca5c567b5cf7434a87ba70ed8e63543
IAPopov <ivan.popov5@dxc.com>
2023-11-26 04:41:47 +0100

    Added foreign keys statements reorder

--- src/main/resources/static/schema_creation.sql
+++ src/main/resources/static/schema_creation.sql
@@ -13,6 +13,7 @@ CREATE TABLE "organization" (
                                 "address_vector_eng" tsvector GENERATED ALWAYS AS (to_tsvector('english', address)) STORED,
                                 "county_id" bigint
 );
+ALTER TABLE "organization" ADD FOREIGN KEY ("county_id") REFERENCES "country" ("id");
 
 CREATE TABLE "contact" (
                            "id" SERIAL PRIMARY KEY,
@@ -22,12 +23,11 @@ CREATE TABLE "contact" (
                            "organization_id" bigint,
                            "value_vector_eng" tsvector GENERATED ALWAYS AS (to_tsvector('english', "value")) STORED
 );
+ALTER TABLE "contact" ADD FOREIGN KEY ("organization_id") REFERENCES "organization" ("id");
 
 CREATE TABLE "organization_common_vector" (
                                             "organization_id" bigint,
                                             "search_vector" tsvector
 );
 
-ALTER TABLE "organization" ADD FOREIGN KEY ("county_id") REFERENCES "country" ("id");
-ALTER TABLE "contact" ADD FOREIGN KEY ("organization_id") REFERENCES "organization" ("id");
 ALTER TABLE "organization_common_vector" ADD FOREIGN KEY ("organization_id") REFERENCES "organization" ("id");
